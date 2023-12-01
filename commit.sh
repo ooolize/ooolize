ff191e2fb02d54c6bb1669f09dfe0e42466ff879
lize <13623436954@163.com>
2023-12-01 15:28:58 +0800

    更新commit脚本

---
+++ commit.sh
@@ -0,0 +1,9 @@
+###
+ # @Description: 
+ # @Author: lize
+ # @Date: 2023-12-01
+ # @LastEditors: lize
+### 
+git config --global https.proxy https://127.0.0.1:17890
+git config --global https.proxy http://127.0.0.1:17890
+git push
